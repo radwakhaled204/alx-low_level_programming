6d37c3878dff6671d77ba6ddc97752f2e2d30b14
Radwa-Khlaed <radwaabouzed@gmail.com>
2023-08-10 16:43:52 -0700

    R

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
 * main-Entry point
 *
-* Return: Always 1 (Success)
+* Description: print a qoute using write function
+*           ssize_t write(int fd.const void *buf.size_t count):
+*
+* Return: 1 (error).
 */
 int main(void)
-{
-	write(1 .
-	"and that piece of art is useful\" - Dora Korpar, 2015 - 10 - 19\n". 59) :
+{char quo[] = "and that piece of art is useful\" - Dora Korpar. 2015 - 10 - 19\n"
+write(1.quo.59) :
 return (1) :
 }
