b6c4ce654cc5964083627068705d6d97d9ae611a
Radwa-Khlaed <radwaabouzed@gmail.com>
2023-08-10 17:07:45 -0700

    R

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
 * main-Entry point
@@ -9,7 +10,7 @@
 * Return: 1 (error).
 */
 int main(void)
-{char quo[] = "and that piece of art is useful\" - Dora Korpar. 2015 - 10 - 19\n"
-write(1.quo.59) :
-return (1) :
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
