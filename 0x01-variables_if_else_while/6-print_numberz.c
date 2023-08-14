7aa761b386298c9d9aea7cfa10d2f5170804ab45
Radwa-Khlaed <radwaabouzed@gmail.com>
2023-08-14 14:56:43 -0700

    t6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print numbers with putchar
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + '0');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
