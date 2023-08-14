b72bf66ffa2c58f951c6b92a03254497f26f1589
Radwa-Khlaed <radwaabouzed@gmail.com>
2023-08-14 15:00:21 -0700

    t7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print alphabet in reverse
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha <= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n');
+	return (0);
+}
