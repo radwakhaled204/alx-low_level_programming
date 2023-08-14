1d6cc5195cced8f11f6f971e3bc9cbb80b211ae4
Radwa-Khlaed <radwaabouzed@gmail.com>
2023-08-14 14:35:29 -0700

    t3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print alphabet
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	char alpha = 'a';
+	char ALPHA = 'A';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+
+	/*UPPERCASE*/
+	while (ALPHA <= 'Z')
+	{
+		putchar(ALPHA);
+		ALPHA++;
+	}
+	putchar("\n");
+	return (0);
+}
