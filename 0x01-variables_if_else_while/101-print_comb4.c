bc56004a5ab586f357592ca803e0c5f4c579a375
Radwa-Khlaed <radwaabouzed@gmail.com>
2023-08-14 16:46:52 -0700

    tadvan11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all of two
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int num1 = 0;
+	int num2, num3;
+
+	while (num1 <= 9)
+	{
+		num2 = 0;
+		while (num <= 9)
+		{
+			num3 = 0;
+			while (num3 <= 9)
+			{
+				if (num1 != num2 &&
+						num1 < num2 &&
+						num2 != num3 &&
+						num2 < num3)
+				{
+					putchar(num1 + 48);
+					putchar(num2 + 48);
+					putchar(num3 + 48);
+
+					if (num1 + num2 + num3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				num3++
+			}
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
