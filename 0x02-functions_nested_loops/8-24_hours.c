ee92fbf71f23453e9178b8da5d8e01e5f9325b48
Radwa-Khlaed <radwaabouzed@gmail.com>
2023-08-18 10:44:18 -0700

    t8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute
+ */
+
+void jack_bauer(void);
+{
+	int hour, min;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hour / 10) + 48);
+			_putchar((hour % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
+	}	
+}
