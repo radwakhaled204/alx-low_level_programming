69faad5ad81b79689aca8330c39305dc96d958a7
Radwa-Khlaed <radwaabouzed@gmail.com>
2023-08-18 14:11:18 -0700

    t0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - function check if c is uppercase
+ *
+ * @c: input for char
+ *
+ * Return: 1 if uppercase. 0 if not
+ */
+
+int _isupper(int c);
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
