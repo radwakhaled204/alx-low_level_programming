48dcc0f0d162087b0b097bcbd37046a8d64251a1
Radwa-Khaled <radwaabouzed@gmail.com>
2023-09-07 04:47:35 -0700

    update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -12,13 +12,8 @@ void print_numbers(void)
 	int i;
 
 	for (i = 0; i <= 9; i++)
-<<<<<<< HEAD
 	{
-		_putchar(num + 48);
-=======
-	{	
 		_putchar(i + 48);
->>>>>>> 06037f2b74f0835624ab716b22bde1cf5abf00f9
-	}
+	{
 	_putchar('\n');
 }
