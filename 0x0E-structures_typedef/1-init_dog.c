4626a88934349efc5d2c211ccb361b49fbdd2165
Radwa-Khaled <131785821+Radwa-Khaled@users.noreply.github.com>
2023-09-22 18:15:24 +0300

    t1\ 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,9 +12,8 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		return (NULL);
+		return;
 	d->name = name;
 	d->owner = owner;
 	d->age = age;
-	return (d);
 }
