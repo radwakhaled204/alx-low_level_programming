56770c8ff90f974e25ad1612c498d7e1875fc7bf
Radwa-Khaled <131785821+Radwa-Khaled@users.noreply.github.com>
2023-09-24 09:51:19 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,45 +2,40 @@
 #include <stdlib.h>
 
 /**
- * main - prints the minimum number of coins to make change for an amount.
+ * main - prints the minimum number of coins.
  * @argc: argument count.
  * @argv: argument vector.
  * Return: 0 if error 1.
  */
 
-int main(int argc, char *argv[])
-{
-	int cents;
-	int coins[] = {25, 10, 5, 2, 1};
-	int num_coins, count, i;
+int main(int argc, char *argv[]) {
+    int cents;
+/**
+ * @c: cousins.
+ * @n_c: num of cousins.
+ */
+    int c[] = {25, 10, 5, 2, 1};
+    int c_n = sizeof(c) / sizeof(c[0]);
+    int count = 0;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
+    if (argc != 2) {
+        printf("Error\n");
+        return 1;
+    }
 
-	cents = atoi(argv[1]);
+    cents = atoi(argv[1]);
 
-	if (cents < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
+    if (cents < 0) {
+        printf("0\n");
+        return 0;
+    }
 
-	num_coins = sizeof(coins) / sizeof(coins[0]);
-	count = 0;
+    for (int x = 0; x < c_n; x++) {
+        count += cents / coins[x];
+        cents %= c[x];
+    }
 
-	for (i = 0; i < num_coins; i++)
-	{
-		while (cents >= coins[i])
-		{
-			cents -= coins[i];
-			count++;
-		}
-	}
+    printf("%d\n", count);
 
-	printf("%d\n", count);
-
-	return (0);
+    return 0;
 }
